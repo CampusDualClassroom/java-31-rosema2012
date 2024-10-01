2cb880c71a5f6845479739b53122c04871b52d95
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-10-01 16:40:06 +0200

    Terminado el ejercicio 31

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,20 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise31 {
-
     public static void main(String[] args) {
+        String filePath = "src/main/resources/lorem.txt";
 
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
 }
